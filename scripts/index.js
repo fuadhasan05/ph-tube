92d57288795a509372fe4e5c6c24bf06b7aca9f1
Fuad Hasan <fuadhasan3503@gmail.com>
2025-03-13 17:28:16 +0600

    added videos by catagories

--- scripts/index.js
+++ scripts/index.js
@@ -10,13 +10,22 @@ function loadVideos() {
     .then((data) => displayVideos(data.videos));
 }
 
+const loadCategoryVideos = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}
+`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category));
+};
+
 function displayCategories(categories) {
   const categoryContainer = document.getElementById("catagory-container");
 
   for (let cat of categories) {
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        <button onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `;
     categoryContainer.append(categoryDiv);
   }
@@ -24,6 +33,8 @@ function displayCategories(categories) {
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
+
+  videoContainer.innerHTML = " ";
 
   videos.forEach((video) => {
     const videoCard = document.createElement("div");
@@ -72,4 +83,3 @@ const displayVideos = (videos) => {
 };
 
 loadCategories();
-loadVideos();
