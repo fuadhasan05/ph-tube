997faefb905607bd1da2abb5b6fcebd06fc128d6
Fuad Hasan <fuadhasan3503@gmail.com>
2025-03-13 15:41:50 +0600

    added videoscontainer

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,72 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
-function displayCategories(categories) {
-    const categoryContainer = document.getElementById("catagory-container");
-
-    for (let cat of categories) {
-        const categoryDiv = document.createElement("div");
-        categoryDiv.innerHTML=`
-        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
-        `
-        categoryContainer.append(categoryDiv);
-    }
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
 }
 
+function displayCategories(categories) {
+  const categoryContainer = document.getElementById("catagory-container");
+
+  for (let cat of categories) {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `;
+    categoryContainer.append(categoryDiv);
+  }
+}
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+
+        <div class="card">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="" />
+          <span
+            class="absolute bottom-2 right-2 text-sm rounded-sm text-white bg-black px-2"
+          >
+            3hrs 56 min ago</span
+          >
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">${video.title}</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+            ${video.authors[0].profile_name}
+              <img class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views}</p>
+          </div>
+        </div>
+      </div>
+
+        `;
+    videoContainer.append(videoCard);
+  });
+};
+
 loadCategories();
+loadVideos();
